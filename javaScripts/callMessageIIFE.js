b7ccfb21649a04d76620b0e2c76e9f705517b9e2
jessica kilby <jessicakilby@gmail.com>
2016-09-14 17:46:01 -0500

    delete function

--- javaScripts/callMessageIIFE.js
+++ javaScripts/callMessageIIFE.js
@@ -4,29 +4,27 @@ var Chatty = (function() {
 	  console.log("An error occurred while transferring");
 	}
 
-	function executeThisCodeWhenChunksArrive () {
-	}
-
 	function executeThisCodeAfterFileLoaded () {
-	  console.log("executeThisCodeAfterFileLoaded it loaded");
+		console.log("executeThisCodeAfterFileLoaded it loaded");
   	  var messageData = JSON.parse(this.responseText);
 	
 		var contentEl = document.getElementById("comment2")
 
-		var outputMessage = "";
+		
   		var storedMessage;
 
 		for (var i = 0; i < messageData.message.length; i++) {
     		storedMessage = messageData.message[i];
-
-    	
-    		outputMessage += "<div>";
-        	outputMessage += `<p> ${storedMessage.message}<button class='delete'>Delete Message</button></p>`;
-      		outputMessage += "</div>";
-      	
+    		console.log(storedMessage);
+        	var insideDiv = `<p> ${storedMessage.message}<button id='delete' class='delete'>Delete Message</button></p>`;
+      		contentEl.innerHTML += "<div>"+ insideDiv +"</div>";
   		};
 
-  		contentEl.innerHTML = outputMessage;
+  		// contentEl.innerHTML = outputMessage;
+	}
+
+	function executeThisCodeWhenChunksArrive () {
+	  
 	}
 
 
